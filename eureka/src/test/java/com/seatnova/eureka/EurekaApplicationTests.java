7ef645467a8ba7eca5ce5feef89d19cba78a5c1e
Prince Khatri <princekhatri1013@gmail.com>
2026-07-03 15:16:12 +0530

    feat: eureka-server-setup

---
+++ eureka/src/test/java/com/seatnova/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatnova.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
